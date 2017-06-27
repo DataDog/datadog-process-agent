6e60a001e548d3d33e5dfc1089a786343acb24e4
Shang Wang <shang.wang@datadoghq.com>
2017-06-27 17:26:06 -0400

    [proc] added containerId field to ProcessStat

--- checks/real_time.go
+++ checks/real_time.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/DataDog/datadog-process-agent/config"
 	"github.com/DataDog/datadog-process-agent/model"
+	"github.com/DataDog/datadog-process-agent/util/docker"
+	log "github.com/cihub/seelog"
 )
 
 func CollectRealTime(cfg *config.AgentConfig, groupID int32) ([]model.MessageBody, error) {
@@ -12,6 +14,14 @@ func CollectRealTime(cfg *config.AgentConfig, groupID int32) ([]model.MessageBod
 	fps, err := process.AllProcesses(cpuDelta, cfg.Concurrency)
 	if err != nil {
 		return nil, err
+	}
+	pids := make([]int32, 0, len(fps))
+	for _, fp := range fps {
+		pids = append(pids, fp.Pid)
+	}
+	containerByPID, err := docker.ContainersByPID(pids)
+	if err != nil && err != docker.ErrDockerNotAvailable {
+		log.Warnf("unable to get docker stats: %s", err)
 	}
 
 	info, err := collectSystemInfo(cfg)
@@ -46,6 +56,7 @@ func CollectRealTime(cfg *config.AgentConfig, groupID int32) ([]model.MessageBod
 			Nice:        fp.Nice,
 			State:       0, //model.ProcessStateFromString(fp.Status), TODO
 			Threads:     fp.NumThreads,
+			ContainerId: cidFromPid(fp.Pid, containerByPID),
 			OpenFdCount: fp.OpenFdCount,
 		})
 	}
@@ -60,4 +71,11 @@ func CollectRealTime(cfg *config.AgentConfig, groupID int32) ([]model.MessageBod
 	})
 
 	return messages, nil
+}
+
+func cidFromPid(pid int32, containerMap map[int32]*docker.Container) string {
+	if c, ok := containerMap[pid]; ok {
+		return c.ID
+	}
+	return ""
 }
