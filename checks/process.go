d5dce58b06f5adfe6afa45cf224de68aa5e59f04
marc921 <marc.brun@datadoghq.com>
2017-08-08 11:45:56 -0400

    Changed from int to float

--- checks/process.go
+++ checks/process.go
@@ -182,19 +182,19 @@ func formatIO(fp *process.FilledProcess, lastIO *process.IOCountersStat, before 
 		return nil
 	}
 	// Reading 0 as a counter means the file could not be opened due to permissions. We distinguish this from a real 0 in rates.
-	readRate := -1
+	readRate := -1.0
 	if fp.IOStat.ReadCount != 0 {
 		readRate = calculateRate(fp.IOStat.ReadCount, lastIO.ReadCount, before)
 	}
-	writeRate := -1
+	writeRate := -1.0
 	if fp.IOStat.WriteCount != 0 {
 		writeRate = calculateRate(fp.IOStat.WriteCount, lastIO.WriteCount, before)
 	}
-	readBytesRate := -1
+	readBytesRate := -1.0
 	if fp.IOStat.ReadBytes != 0 {
 		readBytesRate = calculateRate(fp.IOStat.ReadBytes, lastIO.ReadBytes, before)
 	}
-	writeBytesRate := -1
+	writeBytesRate := -1.0
 	if fp.IOStat.WriteBytes != 0 {
 		writeBytesRate = calculateRate(fp.IOStat.WriteBytes, lastIO.WriteBytes, before)
 	}
