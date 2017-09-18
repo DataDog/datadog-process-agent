a8b4deab81dc5c0b75b616957a07c46a9ce801c6
Conor Branagan <conor.branagan@gmail.com>
2017-09-18 10:42:15 -0400

    Support invalid log levels, fix WARN case

--- config/log_test.go
+++ config/log_test.go
@@ -10,4 +10,7 @@ func TestLogLevelCase(t *testing.T) {
 	assert.NoError(t, NewLoggerLevel("debug", defaultLogFilePath))
 	assert.NoError(t, NewLoggerLevel("InFo", defaultLogFilePath))
 	assert.NoError(t, NewLoggerLevel("INFO", defaultLogFilePath))
+	assert.NoError(t, NewLoggerLevel("WARN", defaultLogFilePath))
+	assert.NoError(t, NewLoggerLevel("WARNING", defaultLogFilePath))
+	assert.NoError(t, NewLoggerLevel("notReal", defaultLogFilePath))
 }
