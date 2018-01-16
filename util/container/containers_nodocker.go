1e111452d90fcdf1983ff39140768391c148641e
Sunny Klair <sunny.klair@datadoghq.com>
2018-01-15 19:39:46 -0500

    Fix no docker container function return types

--- util/container/containers_nodocker.go
+++ util/container/containers_nodocker.go
@@ -6,6 +6,6 @@ import "github.com/DataDog/datadog-agent/pkg/util/docker"
 
 // GetContainers is the unique method that returns all containers on the host (or in the task)
 // and that other agents can consume so that we don't have to convert all containers to the format.
-func GetContainers() ([]*docker.Container, []error) {
-	return *docker.Container{}, []error{}
+func GetContainers() ([]*docker.Container, error) {
+	return *docker.Container{}, nil
 }
