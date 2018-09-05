3ff87f724add196475049d91d684d436aa21316b
Vyacheslav Voronenko <git@voronenko.info>
2018-09-05 12:32:48 +0200

    improve(deployment) publish binary as a separate delivery

--- packaging/publish_staging_package.sh
+++ packaging/publish_staging_package.sh
@@ -4,7 +4,17 @@ if [ -z ${STS_AWS_BUCKET+x} ]; then
 	echo "Missing AGENT_S3_BUCKET in environment"
 	exit 1;
 fi
+
+if [ -z ${PROCESS_AGENT_VERSION+x} ]; then
+	# Pick the latest tag by default for our version.
+	PROCESS_AGENT_VERSION=$(./version.sh)
+	# But we will be building from the master branch in this case.
+fi
+echo $PROCESS_AGENT_VERSION
+FILENAME="process-agent-amd64-$PROCESS_AGENT_VERSION"
 WORKSPACE=${WORKSPACE:-$PWD/../}
 agent_path="$WORKSPACE"
+
+s3cmd put $agent_path/process-agent s3://${STS_AWS_BUCKET:-stackstate-process-agent-test}/binaries/${CIRCLE_BRANCH:-dirty}/$FILENAME
 
 deb-s3 upload --codename ${CIRCLE_BRANCH:-dirty} --bucket ${STS_AWS_BUCKET:-stackstate-process-agent-test} $WORKSPACE/packaging/debian/*.deb
