6e84ceffc1d611434618095ab01d5122c6ef9385
Sami Tabet <sami.tabet@datadoghq.com>
2018-10-10 16:01:14 -0400

    Remove logic to retrieve the env variable DD_SITE when DD_PROCESS_AGENT_URL is not set

--- config/config.go
+++ config/config.go
@@ -463,14 +463,6 @@ func mergeEnvironmentVariables(c *AgentConfig) *AgentConfig {
 		if site := os.Getenv("DD_SITE"); site != "" {
 			log.Infof("Using 'process_dd_url' (%s) and ignoring 'site' (%s)", v, site)
 		}
-	} else if site := os.Getenv("DD_SITE"); site != "" {
-		ep := fmt.Sprintf("https://process.%s", site)
-		url, err := url.Parse(ep)
-		if err != nil {
-			log.Warnf("DD_SITE is invalid site '%s' (transformed into 'https://process.%s'): %s", site, site, err)
-		} else {
-			c.APIEndpoints[0].Endpoint = url
-		}
 	}
 
 	// Process Arguments Scrubbing
