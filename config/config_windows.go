c6fa09966ece4af75ea3d41265d034a9d37fe629
Derek Brown <derek.brown@datadoghq.com>
2018-10-31 16:08:37 -0700

    [windows] fix path generation to dynamically determine installation and config directories rather than hardcode

--- config/config_windows.go
+++ config/config_windows.go
@@ -2,7 +2,14 @@
 
 package config
 
-const (
+import (
+	"path/filepath"
+
+	"github.com/DataDog/datadog-agent/pkg/util/executable"
+	"github.com/DataDog/datadog-agent/pkg/util/winutil"
+)
+
+var (
 	defaultLogFilePath = "c:\\programdata\\datadog\\logs\\process-agent.log"
 
 	// Agent 5
@@ -12,3 +19,15 @@ const (
 	// Agent 6
 	defaultDDAgentBin = "c:\\Program Files\\Datadog\\Datadog Agent\\embedded\\agent.exe"
 )
+
+func init() {
+	pd, err := winutil.GetProgramDataDir()
+	if err == nil {
+		defaultLogFilePath = filepath.Join(pd, "Datadog", "logs", "process-agent.log")
+	}
+	_here, err := executable.Folder()
+	if err == nil {
+		defaultDDAgentBin = filepath.Join(_here, "..", "..", "embedded", "agent.exe")
+	}
+
+}
