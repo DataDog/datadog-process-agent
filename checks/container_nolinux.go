19b0aa0aa48ccb5357de5835abb917d24443052a
Shang Wang <shang.wang@datadoghq.com>
2018-11-21 15:17:09 -0500

    [proc] added dummy function for nolinux container case

--- checks/container_nolinux.go
+++ checks/container_nolinux.go
@@ -44,11 +44,10 @@ func (c *ContainerCheck) Run(cfg *config.AgentConfig, groupID int32) ([]model.Me
 
 // chunkContainers formats and chunks the containers into a slice of chunks using a specific
 // number of chunks. len(result) MUST EQUAL chunks.
-func chunkContainers(
-	ctrList []*containers.Container,
-	lastRates map[string]util.ContainerRateMetrics,
-	lastRun time.Time,
-	chunks int,
-) [][]*model.Container {
+func chunkContainers(ctrList []*containers.Container, lastRates map[string]util.ContainerRateMetrics, lastRun time.Time, chunks int) [][]*model.Container {
 	return make([][]*model.Container, chunks)
+}
+
+func fmtContainers(ctrList []*containers.Container, lastRates map[string]util.ContainerRateMetrics, lastRun time.Time) []*model.Container {
+	return make([]*model.Container, 0)
 }
