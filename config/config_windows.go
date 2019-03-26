fc63d7f65cf72be1dfba91ac3140ab68d4912f03
Sunny Klair <sunny.klair@datadoghq.com>
2019-03-26 16:07:38 -0400

    Remove windows constants

--- config/config_windows.go
+++ config/config_windows.go
@@ -12,10 +12,6 @@ import (
 var (
 	defaultLogFilePath = "c:\\programdata\\datadog\\logs\\process-agent.log"
 
-	// Agent 5
-	defaultDDAgentPy    = "c:\\Program Files\\Datadog\\Datadog Agent\\embedded\\python.exe"
-	defaultDDAgentPyEnv = "PYTHONPATH=c:\\Program Files\\Datadog\\Datadog Agent\\agent"
-
 	// Agent 6
 	defaultDDAgentBin = "c:\\Program Files\\Datadog\\Datadog Agent\\embedded\\agent.exe"
 )
