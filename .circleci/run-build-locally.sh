35cc2925ff3bb5396127fb87f697fb95aa53c95f
Sunny Klair <sunny.klair@datadoghq.com>
2019-05-28 16:28:10 -0400

    Remove all files

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-       --request POST \
-       --form revision=${CURRENT_GIT_REVISION}\
-       --form config=@config.yml \
-       --form notify=false \
-       https://circleci.com/api/v1.1/project/github/DataDog/datadog-process-agent/tree/${CURRENT_GIT_BRANCH}
