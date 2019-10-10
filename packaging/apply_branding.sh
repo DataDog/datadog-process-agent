0cb15f9a06026cd648505f1f0d7ad5378918249e
Zandre Witte <zandre.witte@gmail.com>
2019-10-10 09:40:16 +0200

    add another branding line for default test cases

--- packaging/apply_branding.sh
+++ packaging/apply_branding.sh
@@ -50,7 +50,11 @@ gofmt -l $REPLACE_MODE -r '"dd_agent_py" -> "sts_agent_py"' $REPLACE_SCOPE
 gofmt -l $REPLACE_MODE -r '"dd_agent_py_env" -> "sts_agent_py_env"' $REPLACE_SCOPE
 gofmt -l $REPLACE_MODE -r '"ddconfig" -> "stsconfig"' $REPLACE_SCOPE
 gofmt -l $REPLACE_MODE -r '"https://process.datadoghq.com" -> "http://localhost"' $REPLACE_SCOPE
+gofmt -l $REPLACE_MODE -r '"https://process.datadoghq.eu" -> "http://localhost"' $REPLACE_SCOPE
 gofmt -l $REPLACE_MODE -r '"process.datadoghq.com" -> "localhost"' $REPLACE_SCOPE
+gofmt -l $REPLACE_MODE -r '"process.datadoghq.eu" -> "localhost"' $REPLACE_SCOPE
+gofmt -l $REPLACE_MODE -r '"endpoint=https://process.datadoghq.com,https://process.datadoghq.eu" -> "endpoint=http://localhost,http://localhost"' $REPLACE_SCOPE
+gofmt -l $REPLACE_MODE -r '"    https://process.datadoghq.eu:" -> "    http://localhost:"' $REPLACE_SCOPE
 
 # console changes
 gofmt -l $REPLACE_MODE -r '"/etc/datadog-agent/datadog.yaml" -> "/etc/stackstate-agent/stackstate.yaml"' $REPLACE_SCOPE
