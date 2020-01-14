c4f7d612e0fc29af41aae7188148e2c362de3fbf
Zandre Witte <zandre.witte@gmail.com>
2020-01-14 11:42:10 +0100

    stac-7348 default process blacklist

--- config/config_windows.go
+++ config/config_windows.go
@@ -14,4 +14,33 @@ const (
 )
 
 // Process blacklist
-var defaultBlacklistPatterns = []string{"cmd.exe", "conhost.exe", "DllHost.exe", "dwm.exe", "Explorer.EXE", "lsass.exe", "msdtc.exe", "SearchUI.exe", "sihost.exe", "smartscreen.exe", "svchost.exe", "taskhostw.exe", "tasklist.exe", "VBoxService.exe", "vim.exe", "wininit.exe", "winlogon.exe", "wlms.exe", "wmiprvse.exe", "sshd.exe"}
+var defaultBlacklistPatterns = []string{
+	"stress",
+	"cmd.exe",
+	"conhost.exe",
+	"DllHost.exe",
+	"dwm.exe",
+	"Explorer.EXE",
+	"lsass.exe",
+	"msdtc.exe",
+	"SearchUI.exe",
+	"sihost.exe",
+	"smartscreen.exe",
+	"svchost.exe",
+	"taskhostw.exe",
+	"tasklist.exe",
+	"VBoxService.exe",
+	"vim.exe",
+	"wininit.exe",
+	"winlogon.exe",
+	"wlms.exe",
+	"wmiprvse.exe",
+	"sshd.exe",
+	// Should be ignored, but gets reported with an empty command line
+	//"sppsvc.exe",
+	//"services.exe",
+	//"csrss.exe",
+	//"wininit.exe",
+	//"System",
+	//"smss.exe",
+}
