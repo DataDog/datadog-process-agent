1da977bdf1e950a86bc4b6e8f8c2137c24613265
Zandré Witte <zandre.witte@gmail.com>
2020-07-16 13:13:44 +0200

    fix log message for shortlived relation filtering (#59)

--- config/config.go
+++ config/config.go
@@ -465,7 +465,7 @@ func NewAgentConfig(agentIni *File, agentYaml *YamlAgentConfig, networkYaml *Yam
 	}
 
 	if cfg.EnableShortLivedNetworkRelationFilter {
-		log.Infof("Relation ShortLived filter enabled for connections that are once off and were observed for less than %d seconds", cfg.EnableShortLivedNetworkRelationFilter)
+		log.Infof("Relation ShortLived filter enabled for connections that are once off and were observed for less than %d seconds", cfg.ShortLivedNetworkRelationQualifierSecs)
 	} else {
 		log.Infof("Relation ShortLived filter disabled")
 	}
