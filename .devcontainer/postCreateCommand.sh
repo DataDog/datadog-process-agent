5247c993239a053b7f0209beb381a20aa7745324
Jeroen van Erp <jeroen@hierynomus.com>
2021-04-16 12:03:53 +0200

    STAC-12663: Improve dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sudo chown vscode: /go/src/github.com
 go get github.com/golang/dep/cmd/dep
+go get github.com/awalterschulze/goderive
 dep ensure -v -vendor-only
+go generate ./...
+
+(cd vendor/github.com/gogo/protobuf && make install)
