b64d3fd4fd5e3142cfe7b886123d5b7b7ed03abc
Remco Beckers <remco.beckers@gmail.com>
2022-01-28 13:48:10 +0000

    STAC-0 Fix dev container for go mod

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-go get github.com/golang/dep/cmd/dep
 go get github.com/awalterschulze/goderive
-dep ensure -v -vendor-only
 go generate ./...
 
 sudo apt-get update
 sudo apt-get install rake
-
-(cd vendor/github.com/gogo/protobuf && make install)
