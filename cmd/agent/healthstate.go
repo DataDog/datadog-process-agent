8d317c160c11fd25087af22c8eae9bc39454e39a
Tanja Gasparovic <tanja.gasparovic@ag04.com>
2022-07-14 17:01:34 +0200

    Fixed typo

--- cmd/agent/healthstate.go
+++ cmd/agent/healthstate.go
@@ -76,7 +76,8 @@ func (l *Collector) integrationTopology(check checks.Check) ([]topology.Componen
 			Data: topology.Data{
 				"name":        fmt.Sprintf("%s check on %s", check.Name(), hostname),
 				"integration": check.Name(),
-integ				"tags":        append(commonTags, "agent-integration", fmt.Sprintf("integration-type:%s", check.Name())),
+				"hostname":    hostname,
+				"tags":        append(commonTags, "agent-integration", fmt.Sprintf("integration-type:%s", check.Name())),
 			},
 		},
 	}
